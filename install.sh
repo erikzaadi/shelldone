1f1fbdd2e1038c595a767d6881751c363a0c9714
dsebban <daniel@bigpanda.io>
2018-05-27 14:28:53 +0300

    Add .gitconfig alias and colors

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ ZSH=$(which zsh)
 chsh -s ${ZSH}
 sudo chsh -s ${ZSH}
 
-SYMLINKS=( vim/vimrc tmux/tmux.conf zsh/zshrc )
+SYMLINKS=( vim/vimrc tmux/tmux.conf zsh/zshrc git/.gitconfig)
 for SYMLINK in ${SYMLINKS}; do
     CURRENT=${SCRIPT_BASE}/${SYMLINK}
     BASENAME=$(basename ${CURRENT})
