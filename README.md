bf36d66eae1c42674df28136f2e54daaf575d6b7
erikzaadi <erik.zaadi@gmail.com>
2018-05-27 14:00:52 +0300

    Initial effortz

---
+++ README.md
@@ -0,0 +1,23 @@
+## ShellDone
+
+### Because you need a minimal TUI configuration that just works
+
+#### Installation:
+
+```sh
+./install.sh
+```
+
+#### Tweaking
+
+## TMUX
+
+Modify [tmux conf](./tmux/tmux.conf) as needed.
+
+## VIM
+
+Modify [vimrc](./tmux/vimrc) as needed.
+
+## ZSH
+
+Modify [zshrc](./zsh/zshrc) as needed
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SCRIPT_BASE="$( cd -P "$( dirname "$0" )" && pwd )"
+
+brew install $(cat ./homebrew/brew)
+
+# TODO: check if not in zsh already in ${SHELL}
+
+ZSH=$(which zsh)
+chsh -s ${ZSH}
+sudo chsh -s ${ZSH}
+
+SYMLINKS=( vim/vimrc tmux/tmux.conf zsh/zshrc )
+for SYMLINK in ${SYMLINKS}; do
+    CURRENT=${SCRIPT_BASE}/${SYMLINK}
+    BASENAME=$(basename ${CURRENT})
+    echo "ln -sf ${CURRENT} ~/.${BASENAME}"
+done
+
+# Initialize vim
+
+vim -c 'qa!'
